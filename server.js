e21a0ca2c70c9b5d68cfaa76fd08cf144c285a40
coder-anil01 <145540918+coder-anil01@users.noreply.github.com>
2024-01-07 08:44:38 +0530

    finish-project

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static('frontend/build'))
 
 //******* */ =>  ROUTES
 app.use('/api/v1/auth', authRoute)
